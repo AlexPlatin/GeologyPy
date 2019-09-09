66667b14d44f3763151400fae17045e9fc584c31
AlexPlatin <Alex-1298@yandex.ru>
2019-09-09 21:24:19 +0300

    Update dat_tridb_file.py

--- dat_tridb_file.py
+++ dat_tridb_file.py
@@ -18,7 +18,7 @@ def add_columns_to_dat_file(path, list_fields, str_or_num):
     """
     file = MMpy.File()
     if not file.open(path):
-        raise FileExistsError("Файл не открывается")
+        raise FileExistsError("Can't open file")
     structure = file.structure
     for field, type in zip(list_fields, str_or_num):
         if type == "str":
@@ -27,20 +27,20 @@ def add_columns_to_dat_file(path, list_fields, str_or_num):
             structure.add_field(field, MMpy.FieldType.real, 0, 3)
     file.structure = structure
     file.close()
-    print("Готово")
+    print("Finished")
 
 
 def create_dat_file(path, list_fields, str_or_num):
     """
-    Создание нового .dat файла
-    :param path: полный путь к файлу .dat
-    :param list_fields: список колонок в новом файле.
+    Create new MM.dat file
+    :param path: path .dat
+    :param list_fields: list cols.
 
-    Пример: ["EAST", "NORTH", "RL", "AZ", "NAME"]
+    Example: ["EAST", "NORTH", "RL", "AZ", "NAME"]
 
-    :param str_or_num: список маркировок для колонок - текстовая или численная (str, num)
+    :param str_or_num: type of data (str, num)
 
-    Пример: ["num", "num", "num", "num", "str"]
+    Example: ["num", "num", "num", "num", "str"]
 
     :return: None
     """
@@ -51,37 +51,15 @@ def create_dat_file(path, list_fields, str_or_num):
         elif type == "num":
             structure.add_field(field, MMpy.FieldType.real, 0, 3)
     MMpy.File.create_from_template(path, "", structure)
-    print("Готово")
+    print("Finished")
 
-
-def export_excel(file_in, file_out):
-    """
-    Экспорт .dat файла в Excel (.xls) в оригинальном виде
-    :param file_in: полный путь до файла .dat
-    :param file_out: полный путь до файла .dat
-    :return: None
-    """
-    Export2excel_FormSet20 = MMpy.FormSet("EXPORT2EXCEL", "15.0.5.697")
-    Export2excel_FormSet20.set_field("METADATATITLE", "")
-    Export2excel_FormSet20.set_field("INPUT_FILTER", "")
-    Export2excel_FormSet20.set_field("BOOL_FILTER", "0")
-    Export2excel_FormSet20.set_field("INCLUDEMETADATA", "0")
-    Export2excel_FormSet20.set_field("BOLDHEADER", "1")
-    Export2excel_FormSet20.set_field("INCLUDEHEADER", "1")
-    Export2excel_FormSet20.set_field("SHEETNAME", "Report")
-    Export2excel_FormSet20.set_field("BOOLEXECUTE", "0")
-    Export2excel_FormSet20.set_field("FILETYPEINPUT", "0")
-    Export2excel_FormSet20.set_field("FILENAMEINPUT", file_in)
-    Export2excel_FormSet20.set_field("FILETYPE", "0")
-    Export2excel_FormSet20.set_field("FILENAME", file_out, MMpy.append_flag.none)
-    return Export2excel_FormSet20.run()
 
 
 def get_tridb_names(path_to_tridb):
     """
-    Получение списка имен каркасов из набора
-    :param path_to_tridb: полный путь к набору каркасов
-    :return: список каркасов
+    Get names of wf from wf type
+    :param path_to_tridb: path wf type
+    :return: list of wf
     """
     conn = sqlite3.connect(path_to_tridb)
     cur = conn.cursor()
@@ -94,12 +72,12 @@ def get_tridb_names(path_to_tridb):
 
 def get_coordinates_tridb(path_to_tridb, number_tridb_in_base=-1):
     """
-    Получение координат из набора каркасов
-    :param path_to_tridb: полный путь к набору каркасов
-    :param number_tridb_in_base: если значение -1: получение координат по всем каркасам в наборе
-                                 если значение отличное от -1: получение координат по каркасу под этим номером
-    :return: если значение number_tridb_in_base -1: список из списков координат всех каркасов
-             если значение number_tridb_in_base отличное от -1: список координат каркаса под данным номером
+    Get coordinates from .tridb files
+    :param path_to_tridb: path_wf_type
+    :param number_tridb_in_base: if -1: get all coordinates in wf type
+                                 if not -1: get coordinates of wf with index
+    :return: if number_tridb_in_base -1: all coordinates in wf type
+             if number_tridb_in_base not -1: coordinates of wf with index
     """
     conn = sqlite3.connect(path_to_tridb)
     cur = conn.cursor()
